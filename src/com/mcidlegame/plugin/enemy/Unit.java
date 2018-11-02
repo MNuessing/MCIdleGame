75d677b75f0aeed3059e39556755e00c9006c105
MNuessing <kaiserbutter@gmail.com>
2018-11-02 23:16:52 +0100

    simple unit

---
+++ src/com/mcidlegame/plugin/enemy/Unit.java
@@ -0,0 +1,22 @@
+package com.mcidlegame.plugin.enemy;
+
+public class Unit {
+	private static int lvl = 1;
+	private int hp;
+
+	Unit() {
+		this.hp = lvl ^ 2;
+	}
+
+	public static void increaseLvl() {
+		lvl++;
+	}
+
+	public boolean hit(final int dmg) {
+		this.hp -= dmg;
+		if (this.hp > 0) {
+			return false;
+		}
+		return true;
+	}
+}
