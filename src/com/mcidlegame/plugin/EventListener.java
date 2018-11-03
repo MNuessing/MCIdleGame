c2311b130b67b5d99cadef6f53fe5bbbf53708ef
BuildTools <unconfigured@null.spigotmc.org>
2018-11-03 01:50:34 +0100

    Added Event Handlers for weather and hunger.

--- src/com/mcidlegame/plugin/EventListener.java
+++ src/com/mcidlegame/plugin/EventListener.java
@@ -5,7 +5,12 @@ import org.bukkit.GameMode;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.event.entity.CreatureSpawnEvent;
+import org.bukkit.event.entity.CreatureSpawnEvent.SpawnReason;
+import org.bukkit.event.entity.EntityDamageEvent;
+import org.bukkit.event.entity.FoodLevelChangeEvent;
 import org.bukkit.event.player.PlayerJoinEvent;
+import org.bukkit.event.weather.WeatherChangeEvent;
 
 public class EventListener implements Listener {
 
@@ -15,4 +20,27 @@ public class EventListener implements Listener {
 		player.sendMessage(ChatColor.GREEN + "Welcome.");
 		player.setGameMode(GameMode.ADVENTURE);
 	}
+
+	@EventHandler
+	public void onMobSpawn(final CreatureSpawnEvent event) {
+		if (event.getSpawnReason() != SpawnReason.CUSTOM) {
+			event.setCancelled(true);
+		}
+	}
+
+	@EventHandler
+	public void onDamage(final EntityDamageEvent event) {
+		// TODO: listen hit event here
+	}
+
+	@EventHandler
+	public void onHunger(final FoodLevelChangeEvent event) {
+		event.setCancelled(true);
+	}
+
+	@EventHandler
+	public void onWeather(final WeatherChangeEvent event) {
+		event.setCancelled(true);
+	}
+
 }
