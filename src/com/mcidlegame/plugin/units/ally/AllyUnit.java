02fdbd03cbb9aa0a2dce04f3a2dd7d1cc5e74795
BuildTools <unconfigured@null.spigotmc.org>
2018-11-06 21:08:44 +0100

    Reverted reversion. What the fuck git?

--- src/com/mcidlegame/plugin/units/ally/AllyUnit.java
+++ src/com/mcidlegame/plugin/units/ally/AllyUnit.java
@@ -1,6 +1,9 @@
 package com.mcidlegame.plugin.units.ally;
 
+import java.util.List;
+
 import org.bukkit.Location;
+import org.bukkit.Material;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.meta.ItemMeta;
 import org.bukkit.metadata.FixedMetadataValue;
@@ -26,9 +29,23 @@ public abstract class AllyUnit extends Unit implements Damager {
 	}
 
 	public static AllyUnit fromItem(final ItemStack item, final Location location) {
+		if (item == null) {
+			return null;
+		}
+		// this is and "empty" hand
+		if (item.getType() == Material.AIR) {
+			return null;
+		}
 		final ItemMeta meta = item.getItemMeta();
+		if (!meta.hasDisplayName()) {
+			return null;
+		}
 		final String name = meta.getDisplayName();
-		final String levelString = meta.getLore().get(0);
+		final List<String> lore = meta.getLore();
+		if (lore.isEmpty()) {
+			return null;
+		}
+		final String levelString = lore.get(0);
 		final int level = Integer.parseInt(levelString.substring(7, levelString.length()));
 		return createUnit(name, level, location);
 	}
--- src/com/mcidlegame/plugin/units/ally/PlayerUnit.java
+++ src/com/mcidlegame/plugin/units/ally/PlayerUnit.java
@@ -2,6 +2,11 @@ package com.mcidlegame.plugin.units.ally;
 
 import java.util.function.IntUnaryOperator;
 
+import org.bukkit.entity.Player;
+import org.bukkit.metadata.FixedMetadataValue;
+
+import com.mcidlegame.plugin.Main;
+
 public class PlayerUnit implements Damager {
 
 	private static final IntUnaryOperator damageGrowth = n -> 2 * n;
@@ -17,6 +22,10 @@ public class PlayerUnit implements Damager {
 	@Override
 	public int getDamage() {
 		return this.damage;
+	}
+
+	public static void apply(final Player player) {
+		player.setMetadata(metaString, new FixedMetadataValue(Main.main, new PlayerUnit(player.getLevel())));
 	}
 
 }
